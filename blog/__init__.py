f75b1d48d81c3cf565f93612183e57dd9917b05e
Samrajya Thapa <samrajyathapa@hst000191927um.wifig1.olemiss.edu>
2020-04-13 17:54:36 -0500

    update

--- blog/__init__.py
+++ blog/__init__.py
@@ -7,10 +7,10 @@ def create_app(test_config=None):
     app.secret_key = 'secretkey435273'
     app.config['DEBUG'] = True
     app.config['IMAGE_UPLOADS'] = upload_folder
-    app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-    app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'gasbt435'
-    app.config['MYSQL_DATABASE_DB'] = 'blogapp'
+    app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-iron-east-01.cleardb.net'
+    app.config['MYSQL_DATABASE_USER'] = 'b6be13d48336'
+    app.config['MYSQL_DATABASE_PASSWORD'] = '21c36705'
+    app.config['MYSQL_DATABASE_DB'] = 'heroku_442bfb3107ad0c4'
     app.config['MYSQL_DATABASE_CURSOR'] ='DictCursor'
     mysql.init_app(app)
 
